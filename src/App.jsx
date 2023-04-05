3078387f14af2c84afb5156f1f0e69703d052c15
Earl A. Villapaz <jasonburning16@gmail.com>
2023-04-05 17:58:20 +0800

    jsx structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
 
+import styles from './style';
+
 const App = () => {
   return (
-    <div>App</div>
+    <div className="bg-secondColor w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          Navbar
+        </div>
+      </div>
+
+      <div className={`bg-secondColor ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Hero
+        </div>
+      </div>
+
+      <div className={`bg-secondColor ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Steps
+          Collections
+          About
+          Partners
+        </div>
+      </div>
+    </div>
   );
 };
 
-export default App;
+export default App; 
